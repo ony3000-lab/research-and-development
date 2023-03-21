5d3b3d0e1e84952b3daa62af32ccd2facfbbe6c9
Hyeonjong <nianelo4@gmail.com>
2023-03-21 22:51:59 +0900

    feat: counter 컴포넌트 구현 (partial application pattern)

--- src/3rd-party/Counter/hooks/index.ts
+++ src/3rd-party/Counter/hooks/index.ts
@@ -1,3 +1,4 @@
 export { default as useCounter } from './useCounter';
 export { default as useCounterWithGetters } from './useCounterWithGetters';
 export * from './useCounterWithReducer';
+export { default as useCounterWithComponent } from './useCounterWithComponent';
---
+++ src/3rd-party/Counter/hooks/useCounterWithComponent.tsx
@@ -0,0 +1,64 @@
+import type { ReactNode } from 'react';
+import { useState, useMemo } from 'react';
+import type { CounterContextWithHookState } from '../contexts';
+import { CounterContextWithHook } from '../contexts';
+
+// 재사용
+import {
+  CounterCount,
+  CounterDecrement,
+  CounterIncrement,
+} from '../components/HookedCounter/parts';
+import { CounterLabel } from '../components/CompoundCounter/parts';
+
+type HookedCounterInHookProps = {
+  value: number;
+  children?: ReactNode;
+};
+
+export default function useCounterWithComponent(initialCount: number) {
+  const [count, setCount] = useState(initialCount);
+
+  const decrementHandler = () => {
+    setCount((prevCount) => prevCount - 1);
+  };
+  const incrementHandler = () => {
+    setCount((prevCount) => prevCount + 1);
+  };
+
+  /**
+   * useMemo 등으로 캐싱하지 않으면, hook이 호출될 때마다 기존 컴포넌트를 unmount 하고 새 컴포넌트를 mount한다.
+   */
+  const MemoizedCounter = useMemo(() => {
+    function HookedCounterInHook({ value, children = null }: HookedCounterInHookProps) {
+      const contextState = useMemo<CounterContextWithHookState>(
+        () => ({
+          count: value,
+        }),
+        [value],
+      );
+
+      return (
+        <CounterContextWithHook.Provider value={contextState}>
+          <div className="inline-flex items-center space-x-1 rounded-md border-2 border-solid border-blue-500 dark:border-sky-400">
+            {children}
+          </div>
+        </CounterContextWithHook.Provider>
+      );
+    }
+
+    HookedCounterInHook.Count = CounterCount;
+    HookedCounterInHook.Decrement = CounterDecrement;
+    HookedCounterInHook.Increment = CounterIncrement;
+    HookedCounterInHook.Label = CounterLabel;
+
+    return HookedCounterInHook;
+  }, []);
+
+  return {
+    count,
+    decrementHandler,
+    incrementHandler,
+    Counter: MemoizedCounter,
+  };
+}
