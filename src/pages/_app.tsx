b4da0a3081205a5fdcf3dd617f35d0f0555d2ed0
Hyeonjong <nianelo4@gmail.com>
2023-03-24 19:56:44 +0900

    chore: 전역 아이콘 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,10 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { far } from '@fortawesome/free-regular-svg-icons';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+
+library.add(far, fas);
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
