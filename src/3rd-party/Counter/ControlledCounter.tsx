d341dea37536f66f150cb9239a7d0642913d5866
Hyeonjong <nianelo4@gmail.com>
2023-03-26 13:30:00 +0900

    feat: counter 컴포넌트 수정 (control props pattern)

--- src/3rd-party/Counter/ControlledCounter.tsx
+++ src/3rd-party/Counter/ControlledCounter.tsx
@@ -1,21 +1,31 @@
-import type { ReactNode } from 'react';
-import { useState, useMemo } from 'react';
-import type { CounterContextState } from './contexts';
-import { CounterContext } from './contexts';
+import type { ComponentProps, Dispatch, SetStateAction } from 'react';
+import { createContext, useContext, useState, useMemo, useEffect } from 'react';
 import { CounterContainer } from './layouts';
+import { CounterButton } from './parts';
 
-// 재사용
-import {
-  CounterCount,
-  CounterDecrement,
-  CounterIncrement,
-  CounterLabel,
-} from './components/CompoundCounter/parts';
+type CounterContextState = {
+  count: number;
+  minCount?: number;
+  setMinCount: Dispatch<SetStateAction<number>>;
+  maxCount?: number;
+  setMaxCount: Dispatch<SetStateAction<number>>;
+  decrementHandler: () => void;
+  incrementHandler: () => void;
+};
 
-type ControlledCounterProps = {
+const CounterContext = createContext<CounterContextState>({
+  count: 0,
+  minCount: 0,
+  setMinCount: () => {},
+  maxCount: Infinity,
+  setMaxCount: () => {},
+  decrementHandler: () => {},
+  incrementHandler: () => {},
+});
+
+type ControlledCounterProps = Pick<Parameters<typeof CounterContainer>[0], 'children'> & {
   value: number;
   onChange?: (newCount: number) => void;
-  children?: ReactNode;
 };
 
 function ControlledCounter({
@@ -23,27 +33,31 @@ function ControlledCounter({
   onChange = undefined,
   children = null,
 }: ControlledCounterProps) {
-  const minCount = 0;
-  const [maxCount, setMaxCount] = useState<number>();
+  const [minCount, setMinCount] = useState(0);
+  const [maxCount, setMaxCount] = useState(Infinity);
 
   const contextState = useMemo<CounterContextState>(() => {
     const decrementHandler = () => {
       if (value > minCount) {
-        const newCount = value - 1;
-
-        onChange?.(newCount);
+        onChange?.(value - 1);
       }
     };
     const incrementHandler = () => {
-      if (maxCount === undefined || value < maxCount) {
-        const newCount = value + 1;
-
-        onChange?.(newCount);
+      if (value < maxCount) {
+        onChange?.(value + 1);
       }
     };
 
-    return { count: value, maxCount, setMaxCount, decrementHandler, incrementHandler };
-  }, [maxCount, onChange, value]);
+    return {
+      count: value,
+      minCount,
+      setMinCount,
+      maxCount,
+      setMaxCount,
+      decrementHandler,
+      incrementHandler,
+    };
+  }, [maxCount, minCount, onChange, value]);
 
   return (
     <CounterContext.Provider value={contextState}>
@@ -52,9 +66,63 @@ function ControlledCounter({
   );
 }
 
-ControlledCounter.Count = CounterCount;
-ControlledCounter.Decrement = CounterDecrement;
-ControlledCounter.Increment = CounterIncrement;
-ControlledCounter.Label = CounterLabel;
+type CounterCountProps = {
+  min?: number;
+  max?: number;
+};
 
-export { ControlledCounter };
+ControlledCounter.Count = function CounterCount({ min = 0, max = Infinity }: CounterCountProps) {
+  const { count, minCount, setMinCount, maxCount, setMaxCount } = useContext(CounterContext);
+
+  useEffect(() => {
+    if (minCount !== min) {
+      setMinCount(min);
+    }
+  }, [min, minCount, setMinCount]);
+
+  useEffect(() => {
+    if (maxCount !== max) {
+      setMaxCount(max);
+    }
+  }, [max, maxCount, setMaxCount]);
+
+  return <span className="px-1 font-bold">{count}</span>;
+};
+
+type CounterDecrementProps = {
+  icon?: string;
+};
+
+ControlledCounter.Decrement = function CounterDecrement({ icon = 'minus' }: CounterDecrementProps) {
+  const { decrementHandler } = useContext(CounterContext);
+
+  return (
+    <CounterButton
+      icon={icon}
+      onClick={decrementHandler}
+    />
+  );
+};
+
+type CounterIncrementProps = {
+  icon?: string;
+};
+
+ControlledCounter.Increment = function CounterIncrement({ icon = 'plus' }: CounterIncrementProps) {
+  const { incrementHandler } = useContext(CounterContext);
+
+  return (
+    <CounterButton
+      icon={icon}
+      onClick={incrementHandler}
+    />
+  );
+};
+
+type CounterLabelProps = ComponentProps<'span'>;
+
+ControlledCounter.Label = function CounterLabel({ children }: CounterLabelProps) {
+  return <span className="px-1">{children}</span>;
+};
+
+export default ControlledCounter;
