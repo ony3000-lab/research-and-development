0618cb741e8c0aa76cdabb3095850c5d87b0a978
Hyeonjong <nianelo4@gmail.com>
2023-03-26 19:39:43 +0900

    docs: custom hook pattern 페이지 수정

--- src/pages/design-patterns/custom-hook.tsx
+++ src/pages/design-patterns/custom-hook.tsx
@@ -1,6 +1,5 @@
 import PageHead from '@/components/PageHead';
-import { HookedCounter as Counter } from '@/3rd-party/Counter';
-import { useCounter } from '@/3rd-party/Counter/hooks';
+import { HookedCounter as Counter, useCounter } from '@/3rd-party/Counter';
 
 export default function CustomHookPattern() {
   const {
@@ -31,8 +30,8 @@ export default function CustomHookPattern() {
         <h1>Custom Hook Pattern</h1>
 
         <p>
-          이제 주요 로직은 커스텀 hook으로 이동된다. 이 hook은 여러 내부 로직(state, handler)을
-          노출하여 개발자에게 많은 제어 권한을 부여한다.
+          이제 주요 로직이 커스텀 hook으로 이동한다. 이 hook은 여러 내부 로직(state, handler)을
+          노출하여 개발자에게 뛰어난 제어 기능을 제공한다.
         </p>
 
         <section>
@@ -42,7 +41,7 @@ export default function CustomHookPattern() {
             <li>
               <p>
                 더 많은 제어권 제공: 개발자는 <code>useCounter</code>와 <code>Counter</code> 사이에
-                자체 로직을 삽입해서 <code>Counter</code>의 기본 동작을 수정할 수 있다.
+                자체 로직을 삽입하여 <code>Counter</code>의 기본 동작을 수정할 수 있다.
               </p>
             </li>
           </ul>
@@ -54,9 +53,9 @@ export default function CustomHookPattern() {
           <ul>
             <li>
               <p>
-                구현 복잡성: 로직과 마크업이 분리되어 있으므로, 개발자는 두 부분을 직접 연결해야
-                한다. 따라서, 올바르게 구현하기 위해서는 <code>Counter</code>가 어떻게 동작하는지 잘
-                이해해야 한다.
+                구현 복잡성: 로직과 마크업이 분리되어 있기 때문에 두 부분을 연결하는 것은 개발자의
+                몫이다. 따라서 <code>Counter</code>의 작동 방식을 잘 이해해야 올바르게 구현할 수
+                있다.
               </p>
             </li>
           </ul>
@@ -67,28 +66,40 @@ export default function CustomHookPattern() {
 
           <div>
             <Counter value={firstCount}>
-              <Counter.Decrement onClick={firstDecrementHandler} />
+              <Counter.Decrement
+                icon="minus"
+                onClick={firstDecrementHandler}
+              />
               <Counter.Label>Counter</Counter.Label>
               <Counter.Count />
-              <Counter.Increment onClick={firstIncrementHandler} />
+              <Counter.Increment
+                icon="plus"
+                onClick={firstIncrementHandler}
+              />
             </Counter>
           </div>
 
           <div>
             <Counter value={secondCount}>
-              <Counter.Decrement onClick={doubleDecrementHandler} />
+              <Counter.Decrement
+                icon="minus"
+                onClick={doubleDecrementHandler}
+              />
               <Counter.Label>Double Counter</Counter.Label>
               <Counter.Count />
-              <Counter.Increment onClick={doubleIncrementHandler} />
+              <Counter.Increment
+                icon="plus"
+                onClick={doubleIncrementHandler}
+              />
             </Counter>
           </div>
 
           <div>
             <Counter value={0}>
-              <Counter.Decrement />
+              <Counter.Decrement icon="minus" />
               <Counter.Label>Not Working</Counter.Label>
               <Counter.Count />
-              <Counter.Increment />
+              <Counter.Increment icon="plus" />
             </Counter>
           </div>
         </section>
