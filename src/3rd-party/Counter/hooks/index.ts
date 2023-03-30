ca0404179f093c1b427dc3b5693b7273ac683a30
Hyeonjong <nianelo4@gmail.com>
2023-03-30 23:19:29 +0900

    revert: counter 컴포넌트 제거 (partial application pattern)
    refs: 5d3b3d0

--- src/3rd-party/Counter/hooks/index.ts
+++ src/3rd-party/Counter/hooks/index.ts
@@ -1,4 +1,3 @@
 export { default as useCounter } from './useCounter';
 export { default as useCounterWithGetters } from './useCounterWithGetters';
 export * from './useCounterWithReducer';
-export { default as useCounterWithComponent } from './useCounterWithComponent';
