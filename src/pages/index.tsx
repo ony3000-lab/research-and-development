208352a4c9551daf4803fbd910ead50bbd0de008
Hyeonjong <nianelo4@gmail.com>
2023-04-02 11:27:19 +0900

    docs: 모든 외부 링크 제거하고, 설계 패턴 페이지 링크 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -114,27 +114,9 @@ export default function Home() {
           )}
         >
           <CardLink
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            title="Docs"
-            description="Find in-depth information about Next.js features and API."
-          />
-
-          <CardLink
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            title="Learn"
-            description="Learn about Next.js in an interactive course with quizzes!"
-          />
-
-          <CardLink
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            title="Templates"
-            description="Discover and deploy boilerplate example Next.js projects."
-          />
-
-          <CardLink
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            title="Deploy"
-            description="Instantly deploy your Next.js site to a shareable URL with Vercel."
+            href="/design-patterns"
+            title="Design Patterns"
+            description="리액트 컴포넌트 설계 패턴"
           />
         </div>
       </main>
