9f117a3831a56d17a45ca031166234eb843ae10e
Hyeonjong <nianelo4@gmail.com>
2023-04-07 08:46:31 +0900

    feat: provider 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,20 @@ import '@fortawesome/fontawesome-free/css/all.css';
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import classNames from 'classnames';
+import { RecoilRoot } from 'recoil';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { pretendard } from '@/fonts';
+
+const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <main className={classNames(pretendard.variable, 'font-sans')}>
-      <Component {...pageProps} />
-    </main>
+    <RecoilRoot>
+      <QueryClientProvider client={queryClient}>
+        <main className={classNames(pretendard.variable, 'font-sans')}>
+          <Component {...pageProps} />
+        </main>
+      </QueryClientProvider>
+    </RecoilRoot>
   );
 }
