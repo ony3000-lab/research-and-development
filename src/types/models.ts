cbcebae81722917adf3b6bc310e308f7273959b8
Hyeonjong <nianelo4@gmail.com>
2023-04-08 23:02:07 +0900

    feat: json server의 user model 업데이트

--- src/types/models.ts
+++ src/types/models.ts
@@ -1,10 +1,18 @@
-export type User = {
+type UserBase = {
   id: number;
   objectId: string;
   avatarUrl: string;
   name: string;
   email: string;
   company: string;
+};
+
+export type ClientUser = UserBase & {
   createdAt: Date;
   updatedAt: Date;
+};
+
+export type ServerUser = UserBase & {
+  createdAt: string;
+  updatedAt: string;
 };
